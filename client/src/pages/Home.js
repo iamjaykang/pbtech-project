c337ccc0af1626cf30f14f5f0da419278bbbad08
Kymarndo <49214298+Kymarndo@users.noreply.github.com>
2022-09-20 00:11:10 +1200

    functional carousel with non functional buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,75 @@
-import Navbar from '../components/Navbar';
+import Navbar from "../components/navbar/Navbar";
+import Carousel, { CarouselItem } from "../components/carousel/Carousel";
+import image1 from "../components/carousel/carousel1.png";
+import image2 from "../components/carousel/carousel2.jpg";
+import image3 from "../components/carousel/carousel3.jpg";
+import carouselStyles from "../components/carousel/Carousel.module.css";
 
-function Home () {
-    return (
-        <Navbar />
-    )
+function Home() {
+  return (
+    <>
+      <Navbar />
+      <Carousel>
+        <CarouselItem>
+          <img
+            src={image1}
+            alt=""
+            className={carouselStyles.carouselImage}
+          ></img>
+          <div className={carouselStyles.overlayBG}>
+            <h1 className={carouselStyles.overlayTitle}>
+              Find the best laptop <br /> for your business
+            </h1>
+            <h3 className={carouselStyles.overlayText}>
+              Looking for a perfect laptop for your office? Use our <br />
+              custom tool for prices and features
+            </h3>
+            <button className={carouselStyles.overlayButton}>
+              Find a laptop now
+            </button>
+          </div>
+        </CarouselItem>
+        <CarouselItem>
+          <img
+            src={image2}
+            alt=""
+            className={carouselStyles.carouselImage}
+          ></img>
+          <div className={carouselStyles.overlayBG}>
+            <h1 className={carouselStyles.overlayTitle}>
+              Exclusive deals for <br /> business owners
+            </h1>
+            <h3 className={carouselStyles.overlayText}>
+              Gain access to PB Tech's commercial pricing and <br />
+              features such as leasing and flexibles payments
+            </h3>
+            <button className={carouselStyles.overlayButton2}>
+              Apply for a business account
+            </button>
+          </div>
+        </CarouselItem>
+        <CarouselItem>
+          <img
+            src={image3}
+            alt=""
+            className={carouselStyles.carouselImage}
+          ></img>
+          <div className={carouselStyles.overlayBG}>
+            <h1 className={carouselStyles.overlayTitle}>
+              We're happy to help <br /> with anything
+            </h1>
+            <h3 className={carouselStyles.overlayText}>
+              Let our PB Tech experts help you navigate your tech <br />
+              woes so you can focus on running your business
+            </h3>
+            <button className={carouselStyles.overlayButton3}>
+              Talk to our team
+            </button>
+          </div>
+        </CarouselItem>
+      </Carousel>
+    </>
+  );
 }
 
 export default Home;
